f62d0a2054062a51c4e0489f75486d41e5704344
MTouthang <53576865+MTouthang@users.noreply.github.com>
2023-09-12 23:52:43 +0530

    added user login and template add

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,7 +1,51 @@
+"use client"
+import Link from "next/link"
+import React from "react"
+import  {useRouter} from "next/navigation"
+import  { axios } from 'axios'
+
 export default function SignupPage() {
+
+  const [user, setUser] = React.useState({
+    email: "",
+    password: "", 
+    username: "",
+
+
+  })
+
+  const onSignup = async() => {
+
+  }
+
   return (
-    <div>
-      <h1> Signup</h1>
-    </div>
+   <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <h1> Signup </h1>
+    <hr />
+
+    <label htmlFor=" Username"> Username </label>
+    <input 
+      type="text" 
+      id="username"
+      value = {user.username}
+      onChange={(e) => setUser({...user, username: e.target.value})}
+      placeholder="username"
+      className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
+    />
+
+<label htmlFor="password">password</label>
+        <input 
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
+            id="password"
+            type="password"
+            value={user.password}
+            onChange={(e) => setUser({...user, password: e.target.value})}
+            placeholder="password"
+            />
+  <button
+            onClick={onSignup}
+            className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600">Sign up </button>
+            <Link href="/login">Visit login page</Link>
+   </div>
   )
 }
