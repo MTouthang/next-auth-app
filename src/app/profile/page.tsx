74c9c26008854892fe9fa09b553d70ee65e43a5f
MTouthang <53576865+MTouthang@users.noreply.github.com>
2023-09-16 19:32:01 +0530

    feat: middleware setup with protected route

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,11 +1,35 @@
+"use client"
 import React from 'react'
+import axios from 'axios'
+import Link from "next/link"
+import toast from 'react-hot-toast'
+import { useRouter } from "next/navigation"
 
-const profilePage = () => {
+
+const  ProfilePage = ()  => {
+  const router = useRouter() 
+
+  // remove token id to logout -
+  const logout = async () => {
+    try {
+      await axios.get("/api/users/logout")
+      toast.success("Logout successful")
+      router.push("/login")
+    } catch (error: any) {
+      console.log(error.message)
+      toast.error(error.message)
+    }
+  }
+
+
   return (
-    <div>
+    <div className='flex flex-col items-center justify-center min-h-screen py-2'>
       <h1> Profile dashboard !</h1>
+      <hr />
+      <p> Profile page </p>
+      <button onClick={logout} className='bg-blue-500 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'> Logout </button>
     </div>
   )
 }
 
-export default profilePage
+export default ProfilePage
---
+++ src/middleware.ts
@@ -0,0 +1,35 @@
+import { NextResponse, NextRequest } from "next/server";
+
+// This function can be marked async if using await
+
+export function middleware (request: NextRequest) {
+    const path = request.nextUrl.pathname
+
+    const isPublicPath = path === "/login" || path === "/signup"
+
+    const token = request.cookies.get("token")?.value || ""
+
+    // incase user is login or has token
+    if(isPublicPath && token){
+      return NextResponse.redirect(new URL("/", request.nextUrl))
+    }
+
+    // user has no token 
+    if(!isPublicPath && !token) {
+      return NextResponse.redirect(new URL("/login", request.nextUrl))
+    }
+
+
+}
+
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: [
+    "/",
+    "/profile",
+    "/login",
+    "/signup",
+
+  ]
+}
